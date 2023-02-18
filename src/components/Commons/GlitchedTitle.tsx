36e8912c8b89fecb6bb2b4c43efba5ee41f314af
Blazrr <selmhamedi@gmail.com>
2023-02-18 02:43:22 +0100

    Glitching without hover and on load

--- src/components/Commons/GlitchedTitle.tsx
+++ src/components/Commons/GlitchedTitle.tsx
@@ -1,4 +1,5 @@
 import React, { useRef,useEffect } from "react";
+import { text } from "stream/consumers";
 
 type Props = {
     textValue: string 
@@ -6,40 +7,48 @@ type Props = {
 };
 
 const GlitchedTitle = ({textValue}: Props) => {
-  
+  const textRef = useRef<any>()
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  const toRandom = (event:any) => {
+  const toRandom = () => {
     let iteration = 0;
-  
+    console.log(textRef);
     
     const interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
+      textRef.current.innerText = textRef.current.innerText
         .split("")
         .map((letter:any, index:any) => {
           if(index < iteration) {
-            return event.target.dataset.value[index];
+            return textRef.current.dataset.value[index];
           }
         
           return letters[Math.floor(Math.random() * 26)]
         })
         .join("");
       
-      if(iteration >= event.target.dataset.value.length){ 
+      if(iteration >= textRef.current.dataset.value.length){ 
         clearInterval(interval);
       }
       
       iteration += 1 / 3;
     }, 30);
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      toRandom()
+    }, 5000);
+  
+    return () => clearInterval(interval);
+  }, []);
   
 
   return (
     <div className="flex justify-center ">
       <span
         className="bg-purple-300 p-4 text-7xl font-bold rounded-lg"
-        onMouseEnter={(event:any) => toRandom(event)}
         data-value={textValue}
+        ref={textRef}
       >
         {textValue}
       </span>
